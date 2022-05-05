ae790581e09ffb886c190dcf7a8a323eba65bb1b
bentrd <benjamin@tordjman.com>
2022-05-04 23:49:08 -0400

    updated

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const address = document.getElementById("donationAddress");
+
+const donate = () => {
+    navigator.clipboard.writeText('0x4EdD54039EE4fe332b43477E8E8F233D60059cc2')
+        .then(() => {
+            console.log("address successfully copied");
+        })
+}
